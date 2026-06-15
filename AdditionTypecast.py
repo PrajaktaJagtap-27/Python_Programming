143f2de924f07f621b1dd8fdeeeeb544fdd8e1d1
Prajakta Jagtap <prajaktajagtap274@gmail.com>
2026-06-15 11:36:42 +0530

    Create AdditionTypecast.py

---
+++ AdditionTypecast.py
@@ -0,0 +1,9 @@
+print("Enter First Number :")
+No1 = input()      # input function for the accept number from user
+
+print("Enter Second Number :")
+No2 = input()
+
+Ans = int(No1) + int(No2)       #typecasting  is always temperory
+
+print("Addition is :",Ans)  
