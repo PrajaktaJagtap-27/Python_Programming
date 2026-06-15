57dce064c46fc7d44e6a1cbf3d50411f596b2e31
Prajakta Jagtap <prajaktajagtap274@gmail.com>
2026-06-15 11:36:03 +0530

    Add basic addition functionality in Addition.py
    Implement a simple addition program that takes two user inputs and prints their concatenation.

---
+++ Addition.py
@@ -0,0 +1,9 @@
+print("Enter First Number :")
+No1 = input()      # input function for the accept number from user
+
+print("Enter Second Number :")
+No2 = input()
+
+Ans = No1 + No2
+
+print("Addition is :",Ans)   #output is 1011 for concatination
