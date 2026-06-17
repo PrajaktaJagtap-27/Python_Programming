eb647dfab1499d30020a22077eeded6956376d1a
Prajakta Jagtap <prajaktajagtap274@gmail.com>
2026-06-17 10:01:55 +0530

    Add multiplication sequence in SequenceXX.py

---
+++ SequenceXX.py
@@ -0,0 +1,10 @@
+#sequence 
+print("Enter First Number :")  #execution start from 1 line 
+No1 = int(input())    #typecast
+
+print("Enter Second Number :")
+No2 = int(input())    #typecast
+
+Ans = No1 * No2
+
+print("Multipliction is :",Ans)
