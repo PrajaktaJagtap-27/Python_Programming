098c550c32535c1356451fba5c77d1922e2b76c1
Prajakta Jagtap <prajaktajagtap274@gmail.com>
2026-06-22 14:31:16 +0530

    Implement addition of two numbers in FunctionDemo1.py

---
+++ FunctionDemo1.py
@@ -0,0 +1,17 @@
+#Addition of two number
+
+import Marvellous   #update
+
+def main():
+    print("Enter first  number : ")
+    Value1 = int(input())
+    
+    print("Enter first  number : ")
+    Value2 = int(input())
+
+    Ret = Marvellous.Addition(Value1 , Value2) 
+    print("Addition is :",Ret)
+
+if __name__ =="__main__":   
+    main()
+    
