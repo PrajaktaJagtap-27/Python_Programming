85706627f68c201cff5ff4352627aaa882852411
Prajakta Jagtap <prajaktajagtap274@gmail.com>
2026-07-21 20:26:36 +0530

    Add files via upload

---
+++ OOP5.py
@@ -0,0 +1,32 @@
+class Demo:
+    # class variables 
+   Value1 = 10
+   Value2 = 20
+
+   def __init__(self):
+      self.No1 = 11
+      self.No2 = 21
+
+   #Instance method   it access the all
+   def fun(self):
+      print("Inside instance method  named as fun")
+      print(self.No1)
+      print(self.No2)
+
+      print(self.Value1)
+      print(self.Value2)
+
+   @classmethod
+   def gun(cls):
+      print("Inside the ")
+     
+      #print(Demo.No1)
+      #print(Demo.No2)
+      print(cls.Value1)
+      print(cls.Value2)
+
+#call with object
+
+dobj = Demo()
+dobj.gun()
+
---
+++ OOP7.py
@@ -0,0 +1,36 @@
+class Demo:
+    # class variables 
+   Value1 = 10
+   Value2 = 20
+
+   def __init__(self):
+       self.No1 = 11
+       self.No2 = 21
+
+   #Instance method   it access the all
+   def fun(self):
+       print("Inside instance method  named as fun")
+       print(self.No1)
+       print(self.No2)
+
+       print(self.Value1)
+       print(self.Value2)
+
+   @classmethod
+   def gun(cls):
+       print("Inside the ")
+     
+       #print(Demo.No1)
+       #print(Demo.No2)
+       print(cls.Value1)
+       print(cls.Value2)
+
+   @staticmethod
+   def sun():
+       print("Inside static method  named as sun")
+      
+       print(Demo.Value1)
+       print(Demo.Value2)
+
+Demo.sun()
+
